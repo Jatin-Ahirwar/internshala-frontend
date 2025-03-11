fcf18ba427b586ba6b8efd6c303242236c168902
Jatin-Ahirwar <jatinahirwar24@gmail.com>
2025-03-11 16:56:05 +0530

    change

--- utils/axios.js
+++ utils/axios.js
@@ -2,9 +2,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL:"http://localhost:3080/" || "https://internshala-backend-eceu.onrender.com/",
-    // baseURL: "https://internshala-backend-eceu.onrender.com/",
-    baseURL:"http://localhost:3080/",
+    // baseURL:"http://localhost:3540/" || "https://internshala-backend-eceu.onrender.com/",
+    baseURL: "https://internshala-backend-eceu.onrender.com/",
+    // baseURL:"http://localhost:3080/",
     withCredentials:true
 })
 
